44d27632b18ba32648464d5a2d11aeb37ebf59fe
luanaanicole <luanaanicole03@gmail.com>
2025-09-10 17:18:35 -0300

    Remover pasta duplicada e arquivo zipado

--- config (2)/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Exemplo de rota de teste
-router.get('/', (req, res) => {
-  res.send('Rota de autenticação funcionando!');
-});
-
-module.exports = router;
